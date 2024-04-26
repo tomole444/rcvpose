cec363c0fa138c8e3a6879352213b1f66525f259
irobot <tom.leyh>
2024-04-26 11:08:48 +0200

    More segmentation

--- train.py
+++ train.py
@@ -119,7 +119,7 @@ class Trainer():
                     self.vis.add_scalar('Val_ACC', 
                                         float(torch.sum(torch.where(torch.abs(score_rad-target)[torch.where(target!=0)]<=0.05,1,0)) / float(len(torch.nonzero(target))))
                                         , self.iter_val)
-                    if self.iter_val%50==0:
+                    if self.iter_val%10==0:#%50==0:
                         self.vis.add_image('Val_sem',torch.where(score[0].cpu()>=.5,1,0),self.iter_val)
                 self.iter_val = self.iter_val + 1
         val_loss /= len(self.val_loader)
